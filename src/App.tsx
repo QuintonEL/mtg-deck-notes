f997593de1ea90155b18eba66fe37e4ef9825539
Quinton LaBorde <quintonlaborde@gmail.com>
2025-11-05 15:39:28 -0700

    added in localstorage functionality to save note data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { NewNote } from "./NewNote";
+import { useLocalStorage } from "./useLocalStorage";
 
 export type Note = {
   id: string;
 } & NoteData;
+
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
 
 export type NoteData = {
   title: string;
@@ -17,6 +28,8 @@ export type Tag = {
 };
 
 export default function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
   return (
     <div className="container mx-auto p-4">
       <Routes>

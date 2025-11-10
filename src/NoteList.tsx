0205e1b2c888cbe77f259163af37c19e4cb37e82
Quinton LaBorde <quintonlaborde@gmail.com>
2025-11-10 16:07:32 -0700

    added some logic to loop through and display notes

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,16 +1,30 @@
 import { Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import ReactSelect from "react-select/creatable";
-import { useState } from "react";
-import { Tag } from "./App";
+import { useMemo, useState } from "react";
+import { Note, Tag } from "./App";
 
 type NoteListProps = {
   availableTags: Tag[];
+  notes: Note[];
 };
 
-export function NoteList({ availableTags }: NoteListProps) {
+export function NoteList({ availableTags, notes }: NoteListProps) {
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
   const [title, setTitle] = useState("");
+
+  const filteredNotes = useMemo(() => {
+    return notes.filter((note) => {
+      return (
+        (title === "" ||
+          note.title.toLowerCase().includes(title.toLowerCase())) &&
+        (selectedTags.length === 0 ||
+          selectedTags.every((tag) =>
+            note.tags.some((noteTag) => noteTag.id === tag.id)
+          ))
+      );
+    });
+  }, [title, selectedTags, notes]);
 
   return (
     <>
@@ -123,6 +137,13 @@ export function NoteList({ availableTags }: NoteListProps) {
           </div>
         </div>
       </form>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
+        {filteredNotes.map((note) => (
+          <div key={note.id}>
+            <NoteCard />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
